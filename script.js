2f1a7da85ef497de7c93e53dbc46a2b8745ff3c2
Pilot-Mishari <121152171+Pilot-Mishari@users.noreply.github.com>
2023-04-12 11:17:13 +0000

    Added important buttons

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+
+//information for flight
+const fuelqty = 100 //fuel quantity in x1000 kgs
+const cruiseAlt = 1000000 //estimated cruise altitude in m
+const takeoffSpd = 10000 //takeoff speed in m/s
+const landingSpd = 10000 //landing speed in m/s
+const cruiseSpd = 50000
+
+//irrelavant content
+var currentSpd = {
+    number: 0,
+    string: "0"
+}
+var currentAlt = {
+    number: 0,
+    string: "0"
+}
+var currfuel = fuelqty
+var thrust = {
+    number: 0,
+    string: "0"
+}
+
+var eng0pc = {
+    number: 0,
+    string: "0"
+}
+var eng1pc = {
+    number: 0,
+    string: "0"
+}
+
+var pwr = false
+var gp = true
+var apu = false
+var e0 = false
+var e1 = false
+
+
+console.log(thrust)
+console.log(currentSpd)
+console.log(currentAlt)
+console.log(new String(currentSpd).toString())
+console.log(new String(currentAlt).toString())
+console.log(new String(thrust).toString())
+
+let overSpd = new Audio('sounds/overspeed.mp3')
+
 document.getElementById("main").innerHTML= `
     <div>
         <div class="main-box allStyle">
@@ -9,6 +57,10 @@ document.getElementById("main").innerHTML= `
                     <h2 class=subheader">Comms</h2>
                     <span id="commbox"></span>
                 </div>
+
+                <div class="switchPanels" id="leftSwtichPanel"></div>
+                <div class="switchPanels" id="rightSwtichPanel"></div>
+                <div class="switchPanels" id="centerSwtichPanel"></div>
 
                 <div class="checklist">
                     <h2 class=subheader>Checklist</h2>
@@ -25,7 +77,11 @@ document.getElementById("main").innerHTML= `
         </div>
         <div class="secBox allStyle" id="rbox">
             <div class="main-title">
-                <h2 class="main-title-text">Navigation</h2>   
+                <h2 class="main-title-text">Navigation</h2>  
+            </div>
+            <div id="mainNavContent"></div>
+        </div>
+         
     </div>
 `
 
@@ -69,15 +125,90 @@ document.getElementById("checklistcontent").innerHTML= `
     </div>
 `
 
-document.getElementById("mainInfoContent").innerHTML = `
+
+checkAirSpd();
+
+function increaseAlt(){
+    if(thrust >= 190){
+        thrust = thrust - 10
+        currentAlt = currentAlt + 100
+    }
+
+    else if(thrust <= 190){
+        currentSpd = currentSpd - (thrust + 15)
+        currentAlt = currentAlt + 100
+    }
+}
+
+function increaseAirSpd(){
+    currentSpd = currentSpd + (thrust - 15)
+}
+
+function checkAirSpd(){
+    if(currentSpd >= 1200){
+        overSpd.play()
+    }
+    else if(currentSpd > 1200){
+        overSpd.pause();
+    }
+}
+
+if(pwr == false){
+    document.getElementById("mainInfoContent").innerHTML = `
+        Aircraft Powered Off
+    `
+    document.getElementById("mainNavContent").innerHTML = `
+        Aircraft Powered Off
+    `
+}
+else{}
+
+document.getElementById("leftSwtichPanel").innerHTML = `
+    <div>
+        <button class="mainbtn btn" id="bat" onclick="bat()">Master Pwr</button>
+        <button  class="mainbtn btn" id="eng" onclick="checkgroundpwr()">Ground Power</button>
+    </div>
+`
+
+function bat(){
+    pwr = true
+    console.log(pwr)
+    info()
+}
+
+function checkgroundpwr(){
+    if(gp == true && pwr == true){
+        pwr = true
+        console.log(pwr)
+        info()
+    }
+    else{
+        alert("Ground Power is Not Available")
+    }
+}
+function info(){
+    document.getElementById("mainInfoContent").innerHTML = `
     <div>
         <div class="maninBoxItemsfa">
             <label for="fuelqty">Fuel Quantity</label>:
-            <progress id="fuelqty" value="100" max="100">100</progress>
+            <progress id="fuelqty" value="${currfuel}" max="${fuelqty}">100 |</progress>
 
-            <label for="progress">Mission Progress</label>:
-            <meter id="progress" value="0" max="100" min="0">0 out of 10</progress>
+            <label for="CurrAlt">Altitude</label>:
+            <span class="infoNum" id="currAlt" >${currentAlt.string} m| </span>
+
+            <label for="thrust">Thrust</label>:
+            <span class="infoNum" id="thrust" >${thrust.string}| </span>
+
+            <label for="currentSpd">Current Speed</label>:
+            <span class="infoNum" id="currentSpd" >${currentSpd.string} kn| </span>
+
+            <label for="eng0">Engine 1</label>:
+            <span class="infoNum" id="eng0" >${eng0pc.string}| </span>
+
+            <label for="eng1">Engine 2</label>:
+            <span class="infoNum" id="eng1" >${eng1pc.string}| </span>
         </div>
 
     </div>
 `
+}
